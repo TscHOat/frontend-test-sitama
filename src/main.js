705997f27d977329286e8e229eb62848ffcc143e
TscHOat <tschoat@gmail.com>
2023-03-26 23:52:32 +0700

    update 3/25/2923

--- src/main.js
+++ src/main.js
@@ -4,11 +4,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/main.scss'
+
+// vuetify
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { VDataTable } from 'vuetify/labs/VDataTable'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import CKEditor from '@ckeditor/ckeditor5-vue'
+
+const vuetify = createVuetify({
+  components: { ...components, VDataTable },
+  directives
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
+app.use(CKEditor)
 
 app.mount('#app')
